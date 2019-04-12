11276f242f8ebba8a04df3b8145266e61972db9e
Jason <jbane11@gmail.com>
2019-04-12 15:17:59 -0400

    gitupdate script

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,7 +1,7 @@
 git add *.tex
-git add ./refernces/*
+git add ./references/*.bib
 git add ./images/*
 git add *.pdf
 
 git commit -m "Auto update! "
-git push  https://jbane11:!2255Ball!@github.com/jbane11/latex_shows master
+git push  https://jbane11:!2255Ball!@github.com/jbane11/Thesis master
