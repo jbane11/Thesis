c10023a59531d77a04b65ad4cb55563e4fd64afa
Jason <jbane11@gmail.com>
2019-05-30 12:20:02 -0400

    Auto update!

--- gitupdate.sh
+++ gitupdate.sh
@@ -2,6 +2,7 @@ git add *.tex
 git add ./references/*.bib
 git add ./images/*
 git add *.pdf
+git add *.sh
 
 git commit -m "Auto update! "
-git push  https://jbane11:!2255Ball!@github.com/jbane11/Thesis master
+git push  https://jbane11@github.com/jbane11/Thesis master
