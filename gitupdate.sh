bfa28b2022c8ae00149a89e681b22f6159e47ea3
Jason <jbane11@gmail.com>
2019-08-16 10:01:29 -0400

    Finished chapter 2

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,8 +1,22 @@
+
+UD="Auto Update!"
+
+if [ $# -gt 0 ]
+then
+	UD=$1
+fi
+
+
+
 git add *.tex
 git add ./references/*.bib
 git add ./images/*
 git add *.pdf
 git add *.sh
 
-git commit -m "Auto update! "
+
+
+
+
+git commit -m "$UD"
 git push  https://jbane11@github.com/jbane11/Thesis master
